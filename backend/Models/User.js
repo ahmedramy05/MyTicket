56963e9f1f32d7e1b1e19683785f2f66b729815b
Saif Saad <saifsaad316@gmail.com>
2025-03-06 15:31:01 +0200

    removed irrelvant code in user.js

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,21 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
 
-// MongoDB connection string (replace with your actual connection string)
-/*const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://Mongouser:mongomongo2005@cluster0.yjmil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-    process.exit(1); // Exit the process if the connection fails
-  });*/
-
-// Define the User schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
