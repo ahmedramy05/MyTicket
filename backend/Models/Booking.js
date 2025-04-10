96b043dcd17998c1b5f0f5b0dcb17f1e12190381
Thabettt <thabetology@gmail.com>
2025-04-10 21:42:59 +0200

    added backend paths and files

--- backend/Models/Booking.js
+++ backend/Models/Booking.js
@@ -1,36 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-
-const bookingSchema = new Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', 
-    required: true
+const bookingSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    numberOfTickets: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Confirmed", "Canceled"],
+      default: "Pending",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  eventId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Event', 
-    required: true
-  },
-  numberOfTickets: {
-    type: Number,
-    required: true
-  },
-  totalPrice: {
-    type: Number,
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ["Pending", "Confirmed", "Canceled"],
-    default: "Pending"
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-const Booking = mongoose.model('Booking', bookingSchema);
-module.exports = Booking
-
+  { collection: "Bookings" }
+);
+const Booking = mongoose.model("Booking", bookingSchema);
+module.exports = Booking;
--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  profilePicture: { type: String, default: '' },
+  profilePicture: { type: String, default: "" },
   password: { type: String, required: true },
   role: {
     type: String,
-    enum: ['Standard User', 'Organizer', 'System Admin'],
-    default: 'Standard User',
+    enum: ["Standard User", "Organizer", "System Admin"],
+    default: "Standard User",
   },
   createdAt: { type: Date, default: Date.now },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
